c17d547e9916fe62b91bcb5cc8a5746792552a7f
garchomp009 <91824438+sarveshkanwar@users.noreply.github.com>
2025-03-25 10:05:43 +0530

    uploaded Product.java

---
+++ DynamicOnline_Marketplace/Product.java
@@ -0,0 +1,34 @@
+package java_generics.DynamicOnline_Marketplace;
+
+public class Product<T extends ProductCategory> {
+    private String name;
+    private double price;
+    private T category;
+
+    public Product(String name, double price, T category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public T getCategory() {
+        return category;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void display() {
+        System.out.println(name + " | Category: " + category + " | Price: $" + price);
+    }
+}
+
